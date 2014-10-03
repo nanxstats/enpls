9a262804d735e5bb4ed6bed2d2969b5713d492e7
Xiao Nan <road2stat@gmail.com>
2014-10-04 03:32:01 +0800

    enpls v1.0

---
+++ R/enpls-package.R
@@ -0,0 +1,28 @@
+#' Ensemble Partial Least Squares (EnPLS) Regression
+#'
+#' The enpls package provides functions for running the 
+#' ensemble partial least squares (EnPLS) regression.
+#'
+#' The comprehensive user guide could be opened with 
+#' \code{vignette('enpls')}.
+#'
+#' The enpls package is developed by Computational Biology and 
+#' Drug Design (CBDD) Group, Central South University 
+#' (\url{http://cbdd.csu.edu.cn/}).
+#'
+#' \tabular{ll}{ Package: \tab enpls\cr Type: \tab Package\cr
+#' Version: \tab 1.0\cr License: \tab GPL (>= 2)\cr }
+#'
+#' @name enpls-package
+#' @docType package
+#' @author Nan Xiao <\email{road2stat@@gmail.com}>
+#'         Dongsheng Cao <\email{oriental-cds@@163.com}>
+#'         Qingsong Xu <\email{dasongxu@@gmail.com}>
+#'
+#' @note
+#' Bug reports and feature requests should be sent to
+#' \url{https://github.com/road2stat/enpls/issues}.
+#'
+#' @import pls
+#'
+NULL
